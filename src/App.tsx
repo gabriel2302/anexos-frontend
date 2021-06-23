e2053c7bc777112ba1a0ebf9f4564f0fc13082aa
gabriel2302 <gabriel23002@gmail.com>
2021-06-23 10:59:42 -0300

    Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import {useEffect} from 'react'
 import { GlobalStyle } from './styles/GlobalStyle'
 
 import { Greetings } from './components/Greetings'
+import api from './services/api'
 
+interface Student {
+  id: string;
+  name: string;
+}
 export function App() {
+  const [student, setStudent] = useEffect([]) 
+  useEffect(()=> {
+    api.get('students').then(response => setStudent(response.data))
+  },[]);
   return (
     <>
       <GlobalStyle />
